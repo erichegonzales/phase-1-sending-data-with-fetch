bf2171c93147273f5e3db33da3a01a7907be8c7c
Eriche Gonzales <erichelouise@gmail.com>
2022-07-07 10:46:22 -0400

    commit tree cd348b3fa9310f654a27db18bcec3f1ddccefd7c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+const user = {
+    name: 'Eriche',
+    email: 'eriche@mail.com',
+}
+
+const userObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept-Type': 'application/json'
+    },
+    body: JSON.stringify(user),
+}
+
+const submitData = () => {
+    return fetch('http://localhost:3000/users', userObject)
+    .then(response => response.json())
+    .then(object => console.log(object))
+    .catch(error => console.log('error'))
+}
